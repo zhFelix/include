85637fee1a3ef51c9ee55219518adfdebada3648
Felix <12610306@qq.com>
2024-11-01 20:50:10 +0800

    AES:更改传参方式

--- include/AES.hpp
+++ include/AES.hpp
@@ -3,7 +3,7 @@
 using namespace std;
 class AES {
 	private:
-		char S_box[16][16]={{0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76}, 
+		unsigned char S_Box[16][16]={{0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76}, 
 							{0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0}, 
 							{0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15}, 
 							{0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75}, 
@@ -20,40 +20,72 @@ class AES {
 							{0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf}, 
 							{0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16}};
 		char key[32];
-		void init() {
+		void gen_key() {
 			chrono::system_clock::time_point now = chrono::system_clock::now();
-			mt19937_64 mt(time_since_epoch);
-			uniform_int_distribution<> dist(0, 255);
+			unsigned int seed=chrono::duration_cast<chrono::nanoseconds>(now.time_since_epoch()).count(); 
+			mt19937_64 mt(seed);
+			uniform_int_distribution<> dist(48, 122);
 			for(int i = 0;i<32;i++) {
 				key[i]=dist(mt);
 			}
 		}
-	public:
-		AES() {
-			init();
-		}
-		vector<int> ensub(const char* str) {
-			vector<int> ans;
-			for(int i = 0;i<16;i++) {
-				int l=(str[i]>>4), r=str[i]&15;
-				ans.push_back((int)S_Box[l][r]);
+		vector<vector<int> > char_to_piece(const char* str) {
+			vector<vector<int> > ans(4, vector<int>(4, 0));
+			for(int i = 0;i<4;i++) {
+				for(int j = 0;j<4;j++) {
+					ans[i][j]=str[i*4+j];
+				}
 			}
 			return ans;
 		}
-		const char* desub(const vector<int> str) {
+		const char* piece_to_char(vector<vector<int> > piece) {
 			string ans;
-			for(int i = 0;i<16;i++) {
-				for(int j = 0;j<16;j++) {
-					for(int k = 0;k<16;k++) {
-						if(str[i]==S_Box[j][k]) {
-							ans.push_back((j<<4)+k);
-						}
-					}
+			for(int i = 0;i<4;i++) {
+				for(int j = 0;j<4;j++) {
+					ans+=piece[i][j];
 				}
 			}
 			return ans.c_str();
 		}
-		vector<int> code(const char* msg) {
+		vector<int> piece_to_vector(vector<vector<int> > piece) {
+			vector<int> ans;
+			for(int i = 0;i<4;i++) {
+				for(int j = 0;j<4;j++) {
+					ans.push_back(piece[i][j]);
+				}
+			}
+			return ans;
+		}
+	public:
+		AES() {
+			gen_key();
+		}
+		vector<vector<int> > ensub(const vector<vector<int> > str) {
+			vector<vector<int> > ans(4, vector<int>(4, 0));
+			for(int i = 0;i<4;i++) {
+				for(int j = 0;j<4;j++) {
+					int l=str[i][j]>>4, r=str[i][j]&15;
+					ans[i][j]=(int)S_Box[l][r];
+				}
+			}
+			return ans;
+		}
+		vector<vector<int> > desub(const vector<vector<int> > str) {
+			vector<vector<int> > ans(4, vector<int>(4, 0));
+			for(int i = 0;i<4;i++) {
+				for(int j = 0;j<4;j++) {
+					for(int k = 0;k<16;k++) {
+						for(int l = 0;l<16;l++) {
+							if(str[i][j]==S_Box[k][l]) {
+								ans[i][j]=(k<<4)+l;
+							}
+						}
+					}
+				}
+			}
+			return ans;
+		}
+		vector<int> encode(const char* msg) {
 			string str(msg);
 			vector<int> ans;
 			int len=16-str.length()%16;
@@ -61,11 +93,25 @@ class AES {
 				str.push_back(static_cast<char>(len));
 			}
 			for(int i = 0;i<str.length();i+=16) {
-				vector<int> tmp=ensub(str.substr(i, 16).c_str());
+				vector<int> tmp=piece_to_vector(ensub(char_to_piece(str.substr(i, 16).c_str())));
 				for(int j = 0;j<tmp.size();j++) {
 					ans.push_back(tmp[j]);
 				}
 			}
 			return ans;
+		}
+		const char* decode(const vector<int> msg) {
+			string ans;
+			for(int i = 0;i<msg.size();i+=16) {
+				vector<vector<int> > tmp(4, vector<int>(4, 0));
+				for(int j = 0;j<4;j++) {
+					for(int k = 0;k<4;k++) {
+						tmp[j][k]=msg[j*4+k+i];
+					}
+				}
+				const char* str=piece_to_char(desub(tmp));
+				ans+=str;
+			}
+			return ans.substr(0, ans.length()-ans[ans.length()-1]).c_str();
 		}
 };
--- test/AES.cpp
+++ test/AES.cpp
@@ -3,12 +3,23 @@
 using namespace std;
 int main() {
 	AES aes;
-	vector<int> tmp=aes.code("Hello World!");
-	auto ans=aes.desub(tmp);
-	for(auto i:tmp) {
-		cout<<i<<" ";
+	int mode;
+	scanf("%d", &mode);
+	getchar();
+	if(mode==0) {
+		string msg;
+		getline(cin, msg);
+		vector<int> tmp=aes.encode(msg.c_str());
+		for(int i = 0;i<tmp.size();i++) {
+			cout<<tmp[i]<<" ";
+		}
+	} else {
+		vector<int> str;
+		int x;
+		while(cin>>x) {
+			str.push_back(x);
+		}
+		cout<<aes.decode(str);
 	}
-	cout<<ans;
 	return 0;
 }
-
